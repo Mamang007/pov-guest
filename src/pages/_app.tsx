1b9ed80b59cb246b84ebdad4a9bb01213659dfbf
Ubuntu <ubuntu@localhost.localdomain>
2026-07-23 15:59:33 +0800

    feat: Install Astryx Design System & configure theme (PLR-22)
    - Install @astryxdesign/core, @astryxdesign/theme-neutral, @astryxdesign/cli
    - Run astryx init to generate agent docs
    - Replace vanilla CSS globals with Astryx reset/base/theme imports
    - Wrap app in <Theme> provider with neutralTheme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Theme } from "@astryxdesign/core/theme";
+import { neutralTheme } from "@astryxdesign/theme-neutral";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Theme theme={neutralTheme}>
+      <Component {...pageProps} />
+    </Theme>
+  );
 }
